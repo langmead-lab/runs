fbedca2c4af78dd9946740038cc6df1a1154c65b
Ben Langmead <ben.langmead@gmail.com>
2017-05-25 22:16:51 -0400

    fix preproc script

--- mouseling/marcc_mouseling25_preproc.sh
+++ mouseling/marcc_mouseling25_preproc.sh
@@ -2,19 +2,17 @@
 #SBATCH
 #SBATCH --partition=parallel
 #SBATCH --nodes=1
-#SBATCH --mem=5G
-#SBATCH --time=2:00:00
+#SBATCH --mem=120G
+#SBATCH --time=8:00:00
 #SBATCH --ntasks-per-node=24
 
 NM=mouseling25
 MANIFEST=${NM}.manifest
 RAIL="${HOME}/raildotbio"
 
-mkdir -p ${NM}
-
 python ${RAIL}/rail-rna prep local \
     -m ${MANIFEST} \
-    -o "${NM}/output" \
+    -o "${NM}/preproc" \
     --max-task-attempts 3 \
     --keep-intermediates \
     --log "${NM}/intermediate_log" \
